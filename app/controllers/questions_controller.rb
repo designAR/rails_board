74793d05fad1eb0e188e3aec353a6142f4e5464d
arang choi <arangchoi@arangui-MacBookPro.local>
2024-01-03 16:45:57 +0900

    create_question_#02

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -3,13 +3,25 @@ class QuestionsController < ApplicationController
     end
 
     def show
+        # p params[:id]
+        @question = Question.find(params[:id])
+        # p @question
     end
 
     def new
-        @questions = Question.new
+        @question = Question.new
     end
 
+    # 質問の登録
     def create
+        # p params
+        # p question_params
+        # Questionモデルを初期化
+        @question = Question.new(question_params)
+        # QuetionモデルをDBへ保存
+        @question.save
+        # showレダイレクト
+        redirect_to @question
     end
 
     def edit
@@ -20,5 +32,10 @@ class QuestionsController < ApplicationController
 
     def destroy
     end
+
+    private
+        def question_params
+            params.require(:question).permit(:title, :name, :content)
+        end
 
 end
